e0643ec6635b6817a0a631810ef91c6804280fb9
AnaPolettoo <ana.poletto@edu.pucrs.br>
2026-07-17 17:20:33 -0300

    Initial Commit

---
+++ AcademyAI/AcademyAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcademyAIApp.swift
+//  AcademyAI
+//
+//  Created by Ana Poletto on 17/07/26.
+//
+
+import SwiftUI
+
+@main
+struct AcademyAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AcademyAITests/AcademyAITests.swift
@@ -0,0 +1,17 @@
+//
+//  AcademyAITests.swift
+//  AcademyAITests
+//
+//  Created by Ana Poletto on 17/07/26.
+//
+
+import Testing
+@testable import AcademyAI
+
+struct AcademyAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
